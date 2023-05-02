ff8d455cf0eb2f92b18d2b2883d37234e41699c4
SIDDHARTH <72184804+siddh358@users.noreply.github.com>
2023-05-02 22:16:32 +0530

    Add files via upload

---
+++ MATRIX/MATRIX.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 10;
+
+void printMatrix(int matrix[][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void addMatrices(int matrix1[][MAX_SIZE], int matrix2[][MAX_SIZE], int result[][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+}
+
+void subtractMatrices(int matrix1[][MAX_SIZE], int matrix2[][MAX_SIZE], int result[][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+}
+
+void multiplyMatrices(int matrix1[][MAX_SIZE], int matrix2[][MAX_SIZE], int result[][MAX_SIZE], int rows1, int cols1, int rows2, int cols2) {
+    if (cols1 != rows2) {
+        cout << "Error: incompatible matrix dimensions." << endl;
+        return;
+    }
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            int sum = 0;
+            for (int k = 0; k < cols1; k++) {
+                sum += matrix1[i][k] * matrix2[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+}
+
+void transposeMatrix(int matrix[][MAX_SIZE], int result[][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+}
+
+int main() {
+    int matrix1[MAX_SIZE][MAX_SIZE], matrix2[MAX_SIZE][MAX_SIZE], result[MAX_SIZE][MAX_SIZE];
+    int rows1, cols1, rows2, cols2;
+
+    cout << "Enter the dimensions of matrix 1: ";
+    cin >> rows1 >> cols1;
+    cout << "Enter the elements of matrix 1: " << endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+
+    cout << "Enter the dimensions of matrix 2: ";
+    cin >> rows2 >> cols2;
+    cout << "Enter the elements of matrix 2: " << endl;
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+}
